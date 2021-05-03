2b389c10968fbbeaf2460adf25ae32a60f0e3bde
Brandon-Magofna <magofna68@gmail.com>
2021-05-03 10:13:50 -0700

    update project to include car.csproj file; update car.cs to include methods and car objects

--- Models/car.cs
+++ Models/car.cs
@@ -6,6 +6,11 @@ public class Car
   public string MakeModel;
   public int Price;
   public int Miles;
+
+  public bool WorthBuying(int maxPrice)
+  {
+    return (Price <= maxPrice);
+  }
 }
 
 public class Program
@@ -23,7 +28,7 @@ public class Program
     yugo.Miles = 56000;
 
     Car ford = new Car();
-    ford.MakeModel = "1988 Ford Courtry Squire";
+    ford.MakeModel = "1988 Ford Country Squire";
     ford.Price = 1400;
     ford.Miles = 239001;
 
@@ -31,5 +36,25 @@ public class Program
     amc.MakeModel = "1976 AMC Pacer";
     amc.Price = 400;
     amc.Miles = 198000;
+
+    List<Car> Cars = new List<Car>() { volkswagen, yugo, ford, amc };
+
+    Console.WriteLine("Enter maximum price: ");
+    string stringMaxPrice = Console.ReadLine();
+    int maxPrice = int.Parse(stringMaxPrice);
+
+    List<Car> CarsMatchingSearch = new List<Car>(0);
+    foreach (Car automobile in Cars)
+    {
+      if (automobile.WorthBuying(maxPrice))
+      {
+        CarsMatchingSearch.Add(automobile);
+      }
+    }
+    foreach (Car automobile in CarsMatchingSearch)
+    {
+      Console.WriteLine(automobile.MakeModel);
+    }
   }
+
 }
